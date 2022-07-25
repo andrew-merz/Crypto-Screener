d638900c041e135b57514fe72e1a38e2362308de
andrew-merz <merz.school@gmail.com>
2022-07-25 11:01:36 -0400

    Pages and route boilerplates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import CoinPage from "./Pages/CoinPage";
+import Homepage from "./Pages/Homepage";
 
 function App() {
   return (
-    <div className="App">
-      <H1> Hello World</H1>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route path="/" component={Homepage} exact />
+        <Route path="/coins/:id" component={CoinPage} />
+      </div>
+    </BrowserRouter>
   );
 }
 
