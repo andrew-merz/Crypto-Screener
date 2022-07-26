7c2bad4004b8c3a89949cedd7b5a660e4a0785ca
andrew-merz <merz.school@gmail.com>
2022-07-26 19:48:43 -0400

    material ui styling and themes added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,39 @@
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <AppBar color="transparent" position="static">
       <Container>
         <Toolbar>
-          <Typography>Crypto-Screener</Typography>
+          <Typography
+            onClick={() => navigate("/")}
+            style={{
+              color: "#00FF00",
+              flex: 1,
+              fontWeight: "bold",
+              fontFamily: "Roboto",
+              cursor: "pointer",
+            }}
+            variant="h5"
+          >
+            Crypto-Screener
+          </Typography>
+          <Select style={{ width: 100, height: 40, marginRight: 15 }}>
+            <MenuItem value={"USD"}>USD</MenuItem>
+            <MenuItem value={"CNY"}>CNY</MenuItem>
+            <MenuItem value={"RUB"}>RUB</MenuItem>
+          </Select>
         </Toolbar>
       </Container>
     </AppBar>
