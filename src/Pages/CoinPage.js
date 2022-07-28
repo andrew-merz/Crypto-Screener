e032c0bb65a96049bc3c4dbc059c092dbc3f9aa7
andrew-merz <merz.school@gmail.com>
2022-07-28 18:51:31 -0400

    last commit of the day

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import CryptoState from "../CryptoContext";
 
 const CoinPage = () => {
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
+  const { currency, symbol } = CryptoState();
   return (
     <div>
       <h1>CoinPage</h1>
