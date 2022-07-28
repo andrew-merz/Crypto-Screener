762d1fecc0f21654c1a1d9ad485a80a5fa7f9c9f
andrew-merz <merz.school@gmail.com>
2022-07-28 19:43:30 -0400

    working on single coin page

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import CryptoState from "../CryptoContext";
+import { CryptoState } from "../CryptoContext";
+import { SingleCoin } from "../config/api";
+import CoinInfo from "../components/CoinInfo";
 
 const CoinPage = () => {
   const { id } = useParams();
   const [coin, setCoin] = useState();
   const { currency, symbol } = CryptoState();
+
+  const fetchCoin = async () => {
+    const { data } = await axios.get(SingleCoin(id));
+    setCoin(data);
+  };
+
+  console.log(coin);
+  useEffect(() => {
+    fetchCoin();
+  }, []);
+
   return (
     <div>
-      <h1>CoinPage</h1>
+      <div>
+        <CoinInfo coin={coin} />
+      </div>
     </div>
   );
 };
