a801e675a710574440a96931c04d75eb667bfd09
andrew-merz <merz.school@gmail.com>
2022-07-30 15:58:21 -0400

    chart added

--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -32,22 +32,6 @@ const CoinInfo = ({ coin }) => {
 
   const { currency } = CryptoState();
 
-  const fetchHistoricData = async () => {
-    const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
-    setHistoricData(data.prices);
-  };
-
-  useEffect(() => {
-    fetchHistoricData();
-    // eslint-disable-next-line
-  }, [currency, days]);
-
-  const darkTheme = createTheme({
-    palette: {
-      mode: "dark",
-    },
-  });
-
   const Root = styled("div")(({ theme }) => ({
     width: "75%",
     display: "flex",
@@ -64,37 +48,58 @@ const CoinInfo = ({ coin }) => {
     },
   }));
 
+  const fetchHistoricData = async () => {
+    const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
+    setHistoricData(data.prices);
+  };
+
+  useEffect(() => {
+    fetchHistoricData();
+    // eslint-disable-next-line
+  }, [currency, days]);
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+
   return (
     <ThemeProvider theme={darkTheme}>
       <Root>
-        <div>
-          {!historicData ? (
-            <h4>Loading...</h4>
-          ) : (
-            <div>
-              <Line
-                data={{
-                  labels: historicData.map((coin) => {
-                    let date = new Date(coin[0]);
-                    let time =
-                      date.getHours() > 12
-                        ? `${date.getHours() - 12}:${date.getMinutes()} PM`
-                        : `${date.getHours()}:${date.getMinutes()} AM`;
-                    return days === 1 ? time : date.toLocaleDateString();
-                  }),
+        {!historicData ? (
+          <h4>Loading...</h4>
+        ) : (
+          <>
+            <Line
+              data={{
+                labels: historicData.map((coin) => {
+                  let date = new Date(coin[0]);
+                  let time =
+                    date.getHours() > 12
+                      ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+                      : `${date.getHours()}:${date.getMinutes()} AM`;
+                  return days === 1 ? time : date.toLocaleDateString();
+                }),
 
-                  datasets: [
-                    {
-                      data: historicData.map((coin) => coin[1]),
-                      label: `Price ( Past ${days} Days ) in ${currency}`,
-                      borderColor: "#EEBC1D",
-                    },
-                  ],
-                }}
-              />
-            </div>
-          )}
-        </div>
+                datasets: [
+                  {
+                    data: historicData.map((coin) => coin[1]),
+                    label: `Price ( Past ${days} Days ) in ${currency}`,
+                    borderColor: "#03DAC6",
+                  },
+                ],
+              }}
+              options={{
+                elements: {
+                  point: {
+                    radius: 1,
+                  },
+                },
+              }}
+            />
+          </>
+        )}
       </Root>
     </ThemeProvider>
   );
